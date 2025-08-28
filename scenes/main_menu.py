c0333cb031b41e771b64e93804f7765c6f3d4a1b
RumNCola <jara.fher@gmail.com>
2025-08-28 18:15:36 -0400

    Menu de inicio mi pasion

--- scenes/main_menu.py
+++ scenes/main_menu.py
@@ -10,6 +10,7 @@ from pygame.locals import KEYDOWN, K_RETURN, QUIT, K_ESCAPE
 RED = (255, 0, 0)
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
+PINK= (255,19,255)
 
 
 def gameLoop(CONTINUE_GAME, QUIT_GAME):
@@ -33,21 +34,39 @@ def gameLoop(CONTINUE_GAME, QUIT_GAME):
 
     """ Fondo del menu"""
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    background_image = pygame.image.load("assets/duck_wp.png").convert()
+    background_image = pygame.image.load("assets/mainmenubackground.jpg").convert()
+    background_image = pygame.transform.scale(background_image, (SCREEN_WIDTH, SCREEN_HEIGHT))
 
     """ Texto """
-    font_title = pygame.font.SysFont("arial", 50)
-    title = font_title.render("Menú Principal", True, RED, BLACK)
-    title_rect = title.get_rect(center=(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2))
+    font_title = pygame.font.SysFont("monserrat", 111, bold=True)
+    title = font_title.render("JORGE-WAVE", True, PINK)
+    title_rect = title.get_rect(center=(500, SCREEN_HEIGHT /2 - 250))
 
-    font_text = pygame.font.SysFont("arial", 20)
+    font_title2 = pygame.font.SysFont("monserrat", 110, bold=False)
+    title2 = font_title2.render("JORGE-WAVE", True, WHITE)
+    title_rect2 = title2.get_rect(center=(500+1, SCREEN_HEIGHT /2 - 250))
+
+    #########################
+
+    font_text = pygame.font.SysFont("monserrat", 40)
     text = font_text.render(
-        "Presiona [ENTER] para jugar o [ESC] para salir del juego",
+        "[ENTER] Jugar",
         True,
-        WHITE,
-        BLACK,
+        WHITE
     )
-    text_rect = text.get_rect(center=(SCREEN_WIDTH / 2, (SCREEN_HEIGHT / 2) + 100))
+    text_rect = text.get_rect(center=(500, (SCREEN_HEIGHT / 2) - 75))
+
+    font_head = pygame.font.SysFont("monserrat", 45)
+    head = font_head.render('Menú Principal:', True, WHITE)
+    head_rect = head.get_rect(center=(500, (SCREEN_HEIGHT / 2) - 150))
+
+    font_text1 = pygame.font.SysFont("monserrat", 40)
+    text1 = font_text1.render(
+        "[ESC] Salir del juego",
+        True,
+        WHITE
+    )
+    text_rect1 = text1.get_rect(center=(500, (SCREEN_HEIGHT / 2) + 300))
 
     """ hora de hacer el gameloop """
     # variable booleana para manejar el loop
@@ -60,7 +79,10 @@ def gameLoop(CONTINUE_GAME, QUIT_GAME):
         pygame.display.flip()
         screen.blit(background_image, [0, 0])
         screen.blit(title, title_rect)
+        screen.blit(title2, title_rect2)
         screen.blit(text, text_rect)
+        screen.blit(text1, text_rect1)
+        screen.blit(head, head_rect)
         
         
         # iteramos sobre cada evento en la cola
